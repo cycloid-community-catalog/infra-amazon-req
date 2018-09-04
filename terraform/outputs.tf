b38500658663120ced69b3905161bb27846347ae
Hugo Rosnet <hugo.rosnet@cycloid.io>
2018-09-04 18:11:05 +0200

    git: add initial stack files
    This includes the basic files required to have the stack up & running.

---
+++ terraform/outputs.tf
@@ -0,0 +1,31 @@
+output "req_codecommit_ro_user_id" {
+  value = "${module.req.codecommit_ro_user_id}"
+}
+
+output "req_codecommit_ro_user_arn" {
+  value = "${module.req.codecommit_ro_user_arn}"
+}
+
+output "req_codecommit_username" {
+  value = "${module.req.codecommit_username}"
+}
+
+output "req_codecommit_readonly_username" {
+  value = "${module.req.codecommit_readonly_username}"
+}
+
+output "req_codecommit_repository_id" {
+  value = "${module.req.codecommit_repository_id}"
+}
+
+output "req_remote_state_bucket_name" {
+  value = "${module.req.remote_state_bucket_name}"
+}
+
+output "req_infra_user_id" {
+  value = "${module.req.infra_user_id}"
+}
+
+output "req_infra_user_arn" {
+  value = "${module.req.infra_user_arn}"
+}
