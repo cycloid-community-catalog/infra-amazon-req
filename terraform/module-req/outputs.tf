bd3032baf84bd954abd0f757612b9d2503a90126
Hugo Rosnet <hugo.rosnet@cycloid.io>
2018-09-06 14:04:24 +0200

    terraform: update output format due to count
    As the count is used to control the creation or not of certain elements,
    the output need to include the wildcard, in case more than one of them
    was created.

--- terraform/module-req/outputs.tf
+++ terraform/module-req/outputs.tf
@@ -1,10 +1,10 @@
 # Codecommit repositories
 output "codecommit_ro_user_id" {
-  value = "${aws_iam_user.codecommit_readonly.user_id}"
+  value = "${aws_iam_user.codecommit_readonly.*.user_id}"
 }
 
 output "codecommit_ro_user_arn" {
-  value = "${aws_iam_user.codecommit_readonly.user_arn}"
+  value = "${aws_iam_user.codecommit_readonly.*.user_arn}"
 }
 
 output "codecommit_username" {
@@ -16,19 +16,19 @@ output "codecommit_readonly_username" {
 }
 
 output "codecommit_repository_id" {
-  value = "${aws_codecommit_repository.repository.repository.id}"
+  value = "${aws_codecommit_repository.repository.*.id}"
 }
 
 # S3 remote state bucket
 output "remote_state_bucket_name" {
-  value = "${aws_s3_bucket.terraform_remote_state.id}"
+  value = "${aws_s3_bucket.terraform_remote_state.*.id}"
 }
 
 # IAM infra user
 output "infra_user_id" {
-  value = "${aws_iam_user.infra.user_id}"
+  value = "${aws_iam_user.infra.*.user_id}"
 }
 
 output "infra_user_arn" {
-  value = "${aws_iam_user.infra.user_arn}"
+  value = "${aws_iam_user.infra.*.user_arn}"
 }
